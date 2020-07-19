4ca7b52aa6df91620b923367b1e687f17106c09c
Simon Lüke <semaphor@resolution1.net>
2020-07-19 12:51:51 +0200

    Updated from Riot to Element.

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
+"use strict";
+
 module.exports = Franz => Franz;
--- webview.js
+++ webview.js
@@ -1,24 +1,22 @@
-const path = require('path');
+'use strict';
 
-module.exports = (Franz, options) => {
-  	function getMessages() {
+module.exports = Franz => {
+  function getMessages() {
+    const indirect_badge = document.getElementsByClassName('mx_RoomSubList_badge');
+    let indirect_count = 0;
+    if (indirect_badge.length !== 0) {
+      indirect_count = indirect_badge.length;
+    }
 
-		var indirect_badge = document.getElementsByClassName("mx_RoomSubList_badge");
-		var indirect_count = 0;
-		if(indirect_badge.length != 0)
-			indirect_count = indirect_badge.length;
+    const direct_badge = document.getElementsByClassName('mx_RoomSubList_badgeHighlight');
+    let direct_count = 0;
+    if (direct_badge.length !== 0) {
+      direct_count = direct_badge.length;
+    }
+    // set Franz badge
+    Franz.setBadge(direct_count, indirect_count);
+  }
 
-		var direct_badge = document.getElementsByClassName("mx_RoomSubList_badgeHighlight");
-		var direct_count = 0;
-		if(direct_badge.length != 0)
-			direct_count = direct_badge.length;
-
-    
-    	// set Franz badge
-    	Franz.setBadge(direct_count, indirect_count);
-  	};
-
-  	// check for new messages every second and update Franz badge
-  	Franz.loop(getMessages);
-
+  // check for new messages every second and update Franz badge
+  Franz.loop(getMessages);
 };
